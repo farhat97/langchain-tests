e96175c79fa1bb758696c8aff979bd087346cf2d
Farhat Samara <fsamara@nvisia.com>
2026-01-15 15:07:40 -0600

    Added changes to get elixirs

--- agent.py
+++ agent.py
@@ -1,5 +1,5 @@
 from langchain.agents import create_agent
-from tools import *
+from tools import get_weather, get_all_wizards, get_wizard_elixirs
 from langchain.messages import AIMessage, ToolMessage
 
 # API key stuff
@@ -10,18 +10,26 @@ if not os.environ.get("OPENAI_API_KEY"):
     os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key: ")
 
 
+# Defining state
+# This was an idea for an approach but same result was achieved by better prompting. Leaving here for reference
+# class WizardState(AgentState):
+#     wizards: list[str]
+
 SYSTEM_PROMPT = """
 You are a helpful assistant. You have access to the following tools:
 
 - get_weather: use this to get the weather
 - get_all_wizards: use this to get the list of all wizards
-- get_wizard_potions: use this to get the types of potions that a wizard can use. This requires the user to enter a wizard's name - if no name is provided, request the user to enter a name
+- get_wizard_elixirs: use this to get the types of elixirs that a wizard can use. Use the wizard ID from get_all_wizards results. The result for this query should include the name of the Elixir and the description for it, separated by a colon ":"
+
+When you call get_all_wizards, remember the wizard information for subsequent get_wizard_elixirs calls.
 """
 
 agent = create_agent(
     model="gpt-5-nano",
-    tools=[get_weather, get_all_wizards, get_wizard_potions],
-    system_prompt=SYSTEM_PROMPT
+    tools=[get_weather, get_all_wizards, get_wizard_elixirs],
+    system_prompt=SYSTEM_PROMPT,
+    # state_schema=WizardState
 )
 
 
@@ -32,8 +40,6 @@ def print_response(result_messages):
         if isinstance(msg, ToolMessage):
             print("Tool: " + msg.content)
 
-
-# Run the agent via user prompts
 
 while True:
     print("What would you like me to check?\n")
